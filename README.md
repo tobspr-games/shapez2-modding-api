00de4bdfc43a758001590b3b743a231d66749c87
Lorenzo <lorenzo.kaufmann@tobspr.io>
2023-08-08 17:40:46 -0300

    Adjust paths to use SPZ2_PATH

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ## Shapez Shifter
 
 Official modding API for Shapez2 
+
+#### What is this?
+A basic mod API that will be shipped alongside Shapez2 and will allow players to load mods into their game
+
+#### What this is not?
+This is not 0.1% complete. This is a first proof of concept because we need to start thinking about how we will support modding.
+
--- src/ShapezCallbackExt.cs
+++ src/ShapezCallbackExt.cs
@@ -1,15 +1,24 @@
 ﻿using System;
+using System.Diagnostics;
 using HarmonyLib;
 using JetBrains.Annotations;
 
 public static class ShapezCallbackExt
 {
-    public static Action OnGameStart;
+    public static Action OnPostGameStart;
+    public static Action OnPreGameStart;
+
+    [HarmonyPatch(typeof(GameCore), "Start")]
+    [HarmonyPrefix]
+    static void BeforeGameStart()
+    {
+        OnPreGameStart?.Invoke();
+    }
 
     [HarmonyPatch(typeof(GameCore), "Start")]
     [HarmonyPostfix]
-    private static void CreateOnGameStartCallback()
+    private static void AfterGameStart()
     {
-        OnGameStart?.Invoke();
+        OnPostGameStart?.Invoke();
     }
 }
